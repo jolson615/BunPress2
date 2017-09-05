d80b15560fa24ab6d091498c62c4644ec06f1ef0
jolson615 <jolson615@gmail.com>
2017-09-05 16:58:11 -0500

    finished project

--- BunnyPress2/ViewController.swift
+++ BunnyPress2/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bunPic: UIImageView!
+    @IBOutlet weak var bunButton: UIButton!
+    @IBOutlet weak var bunStatus: UILabel!
+    var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +24,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func bunPress(_ sender: Any) {
+        counter += 1
+        print("You pet the bun \(counter) times!")
+        if counter >= 10 {
+            bunStatus.text = "Okay. You're done. Stop petting the bun."
+            bunPic.image = #imageLiteral(resourceName: "angry-bunny")
+        } else {
+            bunStatus.text = "You pet the bun \(counter) times!"
+        }
+    }
 
-
+    
 
 }
 
